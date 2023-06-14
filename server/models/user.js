457ef2a0ed50c0c3eadcc368ba2a1209eb47d1a6
murugan <muruganvenkatesan55@gmail.com>
2023-06-14 11:05:30 +0530

    Server setup with moongose Basic login signup and protected endpoint creation

--- server/models/user.js
+++ server/models/user.js
@@ -1,28 +1,7 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const passwordComplexity = require('joi-password-complexity');
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  displayName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+// User Schema and Model
+export const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
 });
-
-userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this.id }, process.env.JWTPRIVATEKEY, { expiresIn: '7d' });
-  return token;
-};
-
-const User = mongoose.model('user', userSchema);
-
-const validate = (data) => {
-  const schema = Joi.object({
-    displayName: Joi.string().required().label('Display Name'),
-    email: Joi.email().required().label('Email'),
-    password: passwordComplexity().required().label('Password'),
-  });
-  return schema.validate(data);
-};
-
-module.exports = { User, validate };
---
+++ server/routes/user.js
@@ -0,0 +1,78 @@
+import express from 'express'
+import bcrypt from 'bcrypt';
+import { User } from  '../db/index.js';
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the email is already taken
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "email already exists" });
+    }
+
+    // Generate a salt and hash the password
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Compare the provided password with the stored hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    // Generate a JSON Web Token (JWT)
+    const token = jwt.sign({ userId: user._id }, "secretKey");
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.delete("/:email", async (req, res) => {
+  const { email } = req.params;
+
+  try {
+    // Find and delete the user by email
+    const user = await User.findOneAndDelete({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+export default router
