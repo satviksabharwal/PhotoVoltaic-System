bd3ca44308903efb6cabcd61b9d7537e4d45d8f8
Satvik Sabharwal <satviksabharwal7@gmail.com>
2023-06-22 23:44:45 +0200

    Resolved some routing issues, Improved Product Page UI and other small improvements

--- src/pages/ProductTableContainer.js
+++ src/pages/ProductTableContainer.js
@@ -1,15 +1,41 @@
-import { Table, TableBody, TableContainer, TableHead, TableRow } from "@mui/material";
-import React, { useState } from "react";
+import { IconButton, Table, TableBody, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
 import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { selectCurrentUser } from "../store/user/user.selector";
 import { StyledTableCell, StyledTableRow } from "./TableContainer.styled";
 
 const ProductTableContainer = (productData) => {
-  // const [tableData, setTableData] = useState(productData.productData);
-  console.log(productData?.productData?.length);
+  const currentUser = useSelector(selectCurrentUser);
 
+  const deletehandle = useCallback(async (id) => {
+    try {
+      const url = `http://localhost:5500/api/product/delete/${id}`;
+      const config = {
+        headers: { Authorization: currentUser?.tokenId },
+      };
+      await axios.delete(url, config).then(
+        (response) => {
+          console.log(response);
+          toast.success(response.data.message);
+        },
+        (error) => {
+          toast.error(error.data.message);
+        }
+      );
+    } catch (error) {
+      toast.error(error);
+    }
+  }, []);
+
+  console.log(productData);
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} maxWidth="l" aria-label="customized table">
+      <Table sx={{ minWidth: 700, border: "1px solid #48B2E3" }} maxWidth="l" aria-label="customized table">
         <TableHead>
           <TableRow>
             <StyledTableCell>Product Name</StyledTableCell>
@@ -19,6 +45,9 @@ const ProductTableContainer = (productData) => {
             <StyledTableCell align="right">Area</StyledTableCell>
             <StyledTableCell align="right">Longitude</StyledTableCell>
             <StyledTableCell align="right">Latitude</StyledTableCell>
+            <StyledTableCell align="right">PV Data</StyledTableCell>
+            <StyledTableCell align="right">Edit</StyledTableCell>
+            <StyledTableCell align="right">Delete</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -34,6 +63,20 @@ const ProductTableContainer = (productData) => {
                 <StyledTableCell align="right">{data.area}</StyledTableCell>
                 <StyledTableCell align="right">{data.longitude}</StyledTableCell>
                 <StyledTableCell align="right">{data.latitude}</StyledTableCell>
+                <StyledTableCell align="right">Dummy Data</StyledTableCell>
+                <StyledTableCell align="right" sx={{ m: 0 }}>
+                  <Tooltip title="Click to update Product details.">
+                    <EditIcon sx={{ color: "#48B2E3", mt: 1, cursor: "pointer" }} />
+                  </Tooltip>
+                </StyledTableCell>
+                <StyledTableCell align="right" sx={{ m: 0 }}>
+                  <Tooltip title="Click to delete the Product.">
+                    <DeleteIcon
+                      onClick={() => deletehandle(data.id)}
+                      sx={{ color: "#48B2E3", mt: 1, cursor: "pointer" }}
+                    />
+                  </Tooltip>
+                </StyledTableCell>
               </StyledTableRow>
             ))
           ) : (
